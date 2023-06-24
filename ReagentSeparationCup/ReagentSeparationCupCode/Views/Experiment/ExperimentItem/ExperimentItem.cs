b263a34dcb4132f287aaaea42ec873dbd4bdf09d
黄高林 <1037000546@qq.com>
2023-06-24 23:21:30 +0800

    删除无用代码

--- ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentItem/ExperimentItem.cs
+++ ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentItem/ExperimentItem.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Base;
+﻿using Windows.Base;
 
 namespace ReagentSeparationCupCode.Views
 {
-    internal class ExperimentItem: NotifyBase
+    internal class ExperimentItem : NotifyBase
     {
         public int Index { get; set; }
     }
--- ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentViewModel.cs
+++ ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentViewModel.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using Windows.Base;
 
 namespace ReagentSeparationCupCode.Views
 {
-    internal class ExperimentViewModel:NotifyBase
+    internal class ExperimentViewModel : NotifyBase
     {
         public ExperimentViewModel()
         {
-            ExperimentItems=new ObservableCollection<ExperimentItem>();
+            ExperimentItems = new ObservableCollection<ExperimentItem>();
             for (int i = 0; i < 4; i++)
             {
                 var experimentItem = new ExperimentItem();
@@ -20,6 +15,7 @@ namespace ReagentSeparationCupCode.Views
                 ExperimentItems.Add(experimentItem);
             }
         }
+
         public ObservableCollection<ExperimentItem> ExperimentItems { get; set; }
     }
 }
