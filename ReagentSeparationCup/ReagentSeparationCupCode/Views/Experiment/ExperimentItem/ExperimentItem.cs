fe6ebd915ca3427783f072d918cd893a928e014a
黄高林 <1037000546@qq.com>
2023-06-24 23:18:26 +0800

    U实验部分UI开发

---
+++ ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentItem/ExperimentItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Base;
+
+namespace ReagentSeparationCupCode.Views
+{
+    internal class ExperimentItem: NotifyBase
+    {
+        public int Index { get; set; }
+    }
+}
---
+++ ReagentSeparationCup/ReagentSeparationCupCode/Views/Experiment/ExperimentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Base;
+
+namespace ReagentSeparationCupCode.Views
+{
+    internal class ExperimentViewModel:NotifyBase
+    {
+        public ExperimentViewModel()
+        {
+            ExperimentItems=new ObservableCollection<ExperimentItem>();
+            for (int i = 0; i < 4; i++)
+            {
+                var experimentItem = new ExperimentItem();
+                experimentItem.Index = i + 1;
+                ExperimentItems.Add(experimentItem);
+            }
+        }
+        public ObservableCollection<ExperimentItem> ExperimentItems { get; set; }
+    }
+}
